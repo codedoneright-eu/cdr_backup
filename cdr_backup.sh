f3aaaa5a6ff2e116b2547fc01af4ac16e5921c3a
codedoneright-eu <61863034+codedoneright-eu@users.noreply.github.com>
2021-01-25 16:03:11 +0100

    Add files via upload

--- cdr_backup.sh
+++ cdr_backup.sh
@@ -1,112 +1,112 @@
 #!/bin/bash
-set -e
-
-clear
 
 DATE="$(which date)"
 TIMESTAMP=`$DATE '+%Y.%m.%d'`
 
 export WORK_DIR=`pwd`
 
-echo "Hello $USER, Please follow the on-screen prompts"
 echo ""
-echo "This script helps in creating and restoring backups of any drive"
+echo "Linux Backup and Restore Helper"
+
 echo ""
-echo "Caution! Please be careful when restoring backups as you may easily"
-echo "destroy your data. Ensure that you are writing to a correct drive!"
-echo "You will need sudo in order to use this script"
+echo "Hello $USER, Please follow on-screen instructions"
 echo ""
-echo "The working folder is $WORK_DIR"
-echo "To change directory quit the script, navigate to your"
-echo "designated backup folder and run the script again"
+echo "This script creates and restores backups of a drive, sudo is required"
+echo "To find out drive letters run 'sudo fdisk -l' and identify your drive"
+echo "Script works from current working directory."
 echo ""
-echo "Please select function:"
-echo "b - Make a backup"
-echo "r - Restore a backup"
-echo "q - quit"
+echo "Please select function..."
+echo "b: Make a backup"
+echo "r: Restore a backup"
+echo "q: quit"
 echo ""
-read -p "What would you have me do (b/r/q) " PART
+
+read -p "What would you like to do (b/r/q): " PART
 
 if [ $PART = b ]; then
 
     echo ""
-    echo "This part will help you make a backup of a drive. Which one would you like"
-    read -p "to clone? Please provide the letter of sdX " DRIVE_B
+    echo "This part will make a backup." 
+    echo "Which device would you like to clone?"
+    
+    read -p "Please provide the letter (sdX): " DRIVE_B
+    
     echo ""
-    echo "I will make a clone of /dev/sd${DRIVE_B}"
+    echo "Cloning /dev/sd${DRIVE_B}"
     echo ""
-    echo "Output file will be named NAME_$TIMESTAMP.zip"
-    read -p "What should I put as NAME? " NAMEB
+    echo "Output file name structure is NAME_${TIMESTAMP}.zip"
+    
+    read -p "Please specify NAME part: " NAMEB
+    
     echo ""
-    echo "The file will be named ${NAMEB}_${TIMESTAMP}.zip"
+    echo "Output file will be named ${NAMEB}_${TIMESTAMP}.zip"
     echo ""
-    echo "Starting the procedure. Please wait... this may take a while"
+    echo "This will take a while. Please wait... "
+    echo ""
 
-    sudo dd if=/dev/sd${DRIVE_B} of=${NAMEB}_${TIMESTAMP}.img bs=4M conv=noerror status=progress
-
-    echo ""
-    echo "Compressing ${NAMEB}_$TIMESTAMP.img. Please wait... this may take a while"
-    zip ${NAMEB}_$TIMESTAMP.zip ${NAMEB}_$TIMESTAMP.img
-
-    echo ""
-    echo "Removing ${NAMEB}_$TIMESTAMP.img, we do not need that anymore"
-    sudo rm ${NAMEB}_$TIMESTAMP.img
-
-    SIZE="$(du -h ${NAMEB}_$TIMESTAMP.zip | awk '{ print $1 }')"
+    sudo dd if=/dev/sd${DRIVE_B} of=${NAMEB}_${TIMESTAMP}.img conv=noerror status=progress
 
     echo ""
-    echo "$USER, I finished. The file ${NAMEB}_$TIMESTAMP.zip is about $SIZE."
-    echo "Full file path is ${WORK_DIR}/${NAMEB}_$TIMESTAMP.zip"
+    echo "Compressing ${NAMEB}_${TIMESTAMP}.img, please wait..."
+    
+    zip ${NAMEB}_$TIMESTAMP.zip ${NAMEB}_${TIMESTAMP}.img
+
+    echo ""
+    echo "Removing ${NAMEB}_${TIMESTAMP}.img"
+    
+    sudo rm ${NAMEB}_${TIMESTAMP}.img
+
+    SIZE="$(du -h ${NAMEB}_${TIMESTAMP}.zip | awk '{ print $1 }')"
+
+    echo ""
+    echo "The file ${NAMEB}_${TIMESTAMP}.zip is about ${SIZE}."
+    echo "Full file path is ${WORK_DIR}/${NAMEB}_{$TIMESTAMP}.zip"
 
 elif [ $PART = r ]; then
 
     echo ""
-    echo "This part will help you restore a backup of a drive, it will be"
-    echo "restored from the current directory. You will be asked to provide"
-    echo "the file name of the backup that you want to restore and"
-    echo "the letter of the drive that you want to write to"
+    echo "This part will restore a backup."
     echo ""
-    echo "The file name has the following pattern NAME_YYYY.MM.DD.zip"
-    echo "You will be asked for parts of the name in the next step"
+    echo "Script looks for input file with the following pattern NAME_YYYY.MM.DD.zip"
     echo ""
-    echo "To which drive would you like to write?"
-    read -p "Please provide the letter of sdX " DRIVE_R
+    echo "To which device would you like to write?"
+    
+    read -p "Please provide the letter (sdX): " DRIVE_R
+    
     echo ""
-    echo "Which file you want to use?"
-    read -p "Please specify NAME part of the name " NAMER
-    read -p "Please specify YYYY part of the name " YEAR
-    read -p "Please specify MM   part of the name " MONTH
-    read -p "Please specify DD   part of the name " DAY
+    echo "Which file you want to restore?"
+    
+    read -p "Please specify NAME part of the name: " NAMER
+    read -p "Please specify YYYY part of the name: " YEAR
+    read -p "Please specify MM   part of the name: " MONTH
+    read -p "Please specify DD   part of the name: " DAY
 
     echo ""
     echo "Unzipping ${NAMER}_${YEAR}.${MONTH}.${DAY}.zip" 
-    echo "Please wait... this may take a while"
+    echo "This will take a while. Please wait..."
+    echo ""
+    
     unzip ${NAMER}_${YEAR}.${MONTH}.${DAY}.zip
 
     echo ""
-    echo "Writing image ${NAMER}_${YEAR}.${MONTH}.${DAY}.img to /dev/sd${DRIVE_R}"
-    echo "Please wait... this may take a while"
-    sudo dd if=${NAMER}_${YEAR}.${MONTH}.${DAY}.img of=/dev/sd${DRIVE_R} bs=4M conv=noerror status=progress
+    echo "Writing ${NAMER}_${YEAR}.${MONTH}.${DAY}.img to /dev/sd${DRIVE_R}"
+    echo "This will take a while. Please wait..."
+    
+    sudo dd if=${NAMER}_${YEAR}.${MONTH}.${DAY}.img of=/dev/sd${DRIVE_R} conv=noerror status=progress
 
     echo ""
-    echo "Removing ${NAMER}_${YEAR}.${MONTH}.${DAY}.img, we do not need that anymore"
+    echo "Removing ${NAMER}_${YEAR}.${MONTH}.${DAY}.img"
+    
     sudo rm ${NAMER}_${YEAR}.${MONTH}.${DAY}.img
 
     echo ""
-    echo "$USER, I finished. Drive /dev/sd${DRIVE_R} is ready for use"
-
-elif [ $PART = q ]; then
-
-    echo ""
-    echo "You have chosen to end the script"
+    echo "Drive /dev/sd${DRIVE_R} is ready for use"
 
 else
 
     echo ""
-    echo "Error! Wrong selection. Please note that selection is case sensitive."
-    echo "Please run the script again and chose correct function."
 
 fi
 
 echo ""
-echo "Thank you for using my services and have a good day!"
+echo "Done."
